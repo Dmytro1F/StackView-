e5de04e1e04cedd59e5112a959875dbbb9cbac7a
Dmytro <dmytro.fedorov@icloud.com>
2016-09-19 07:08:00 +0400

    Initial Commit

---
+++ StackView)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackView)
+//
+//  Created by Dmytro Fedorov on 9/19/16.
+//  Copyright © 2016 Dmytro Fedorov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
